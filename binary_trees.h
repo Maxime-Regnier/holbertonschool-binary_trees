233c05e3a23f2036d891f3dda48008fc31bea6e4
Maxime-Regnier <11493@holbertonstudents.com>
2025-12-08 11:59:54 +0100

    "Define binary_tree_t structure"

--- binary_trees.h
+++ binary_trees.h
@@ -3,6 +3,14 @@
 
 #include <stddef.h>
 
+typedef struct binary_tree_s
+{
+    int n;                        // La valeur du noeud
+    struct binary_tree_s *parent;  // Pointeur vers le parent
+    struct binary_tree_s *left;    // Pointeur vers l'enfant gauche
+    struct binary_tree_s *right;   // Pointeur vers l'enfant droit
+} binary_tree_t;
+
 int _putchar(char c);
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value);
