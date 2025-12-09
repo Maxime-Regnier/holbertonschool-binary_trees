269d75876bd302872ca189dd2dce8cfcc338d48e
Maxime-Regnier <11493@holbertonstudents.com>
2025-12-09 10:02:19 +0100

    "feat: add binary_tree_node, update prototypes, and test main"

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -1,4 +1,5 @@
-#include
+#include <stdlib.h>
+#include "binary_trees.h"
 /**
  * 
  * 
@@ -6,5 +7,19 @@
  */
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
+binary_tree_t *new_node;
 
+    /* 1. Allouer la mémoire pour le nouveau nœud */
+new_node = malloc(sizeof(binary_tree_t));
+
+    /* 2. Vérifier l'allocation */
+if (new_node == NULL)
+{
+return (NULL);
+}
+new_node->n = value;
+new_node->parent = parent;
+new_node->left = NULL;
+new_node->right = NULL;
+return (new_node);
 }
