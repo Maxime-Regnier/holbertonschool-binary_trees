b294318fdd2f3071cb87305311d1e755c71efd42
Maxime-Regnier <11493@holbertonstudents.com>
2025-12-10 09:48:23 +0100

    "Add Betty documentation with clear description of parameters, behavior, and return value"

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -1,9 +1,14 @@
 #include "binary_trees.h"
 #include <stdlib.h>
 /**
- * 
- * 
- * 
+ * binary_tree_insert_right - Inserts a node as the right-child of a parent
+ * @parent: Pointer to the parent node
+ * @value: Value to store in the new node
+ *
+ * Description: If the parent already has a right child, it is moved
+ * to be the right child of the newly created node.
+ *
+ * Return: Pointer to the new node, or NULL on failure
  */
 binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 {
